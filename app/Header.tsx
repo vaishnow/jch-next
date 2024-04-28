a34594eab2781242cec6e0aca09c68f80797f842
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-04-28 16:05:40 +0530

    add link to homepage on navbar

--- app/Header.tsx
+++ app/Header.tsx
@@ -26,48 +26,28 @@ export default function Header() {
   }, []);
 
   return (
-    <header>
-      <nav
-        className={`fixed z-20 px-5 flex inset-x-0 mx-auto justify-between font-semibold duration-500 ${
-          isScrolled
-            ? `w-11/12 py-1 top-2 bg-slate-100 shadow rounded-lg`
-            : `w-full py-3`
-        }`}
-      >
-        <Link href="/" className="flex items-center space-x-1 text-jp-red">
+    <header >
+      <nav className={`fixed z-20 px-5 flex inset-x-0 mx-auto justify-between font-semibold duration-500 ${isScrolled?`w-11/12 py-1 top-2 bg-slate-100 shadow rounded-lg`:`w-full py-3`}`}>
+        <Link href='/' className="flex items-center space-x-1 text-jp-red">
           <Image src={logo} alt="JCH Logo" className="size-14" />
-          <h1 className={`${style.className} text-4xl text-nowrap`}>
-            Japan Career Hub
-          </h1>
+          <h1 className={`${style.className} text-4xl text-nowrap`}>Japan Career Hub</h1>
         </Link>
         <div className="text-white flex items-center space-x-2">
           <Link
             href="/jobs"
-            className={`inline-block w-28 text-center p-1 backdrop-blur-[2px] rounded-md hover:shadow-lg ${
-              isScrolled
-                ? `text-black`
-                : `bg-[#0005] hover:backdrop-blur-sm hover:bg-[#0008]`
-            }`}
+            className={`inline-block w-28 text-center p-1 backdrop-blur-[2px] rounded-md hover:shadow-lg ${isScrolled?`text-black`:`bg-[#0005] hover:backdrop-blur-sm hover:bg-[#0008]`}`}
           >
             Jobs
           </Link>
           <Link
             href="/about"
-            className={`inline-block w-28 text-center p-1 backdrop-blur-[2px] rounded-md hover:shadow-lg ${
-              isScrolled
-                ? `text-black`
-                : `bg-[#0005] hover:backdrop-blur-sm hover:bg-[#0008]`
-            }`}
+            className={`inline-block w-28 text-center p-1 backdrop-blur-[2px] rounded-md hover:shadow-lg ${isScrolled?`text-black`:`bg-[#0005] hover:backdrop-blur-sm hover:bg-[#0008]`}`}
           >
             About
           </Link>
           <Link
             href="/contact"
-            className={`inline-block w-28 text-center p-1 backdrop-blur-[2px] rounded-md hover:shadow-lg ${
-              isScrolled
-                ? `text-black`
-                : `bg-[#0005] hover:backdrop-blur-sm hover:bg-[#0008]`
-            }`}
+            className={`inline-block w-28 text-center p-1 backdrop-blur-[2px] rounded-md hover:shadow-lg ${isScrolled?`text-black`:`bg-[#0005] hover:backdrop-blur-sm hover:bg-[#0008]`}`}
           >
             Contact
           </Link>
